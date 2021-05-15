8ff614cc019c36f245faf750eaba12e7f57b1736
jpm <jpm9511@gmail.com>
2021-05-14 20:19:53 -0600

    Robustness and documentation changes to reels source files.

--- reels.c
+++ reels.c
@@ -1,7 +1,11 @@
+// File: reels.c
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include "reels.h"
+
+// PUBLIC VARIABLES
 
 // Use fixed reels (pre-shuffled w/ correct symbol counts) or not
 bool use_fixed_reels = TRUE;
@@ -9,10 +13,12 @@ bool use_fixed_reels = TRUE;
 // Pointers to each reel
 symbol *reels[N_REELS];
 
-//  REEL SIZES FOR EACH REEL
+//  Reel sizes for each reel.
 const int reel_sizes[N_REELS] = {47, 46, 48, 50, 50};
 
-// LM962 SYMBOL COUNTS PER REEL
+// PRIVATE VARS
+
+// LM962 symbol counts per reel
 static const int reel_cnts[N_REELS][SYM_SET_SIZE] = {
     {2, 4, 4, 6, 5, 6, 6, 5, 5, 2, 2},
     {2, 4, 4, 4, 4, 4, 6, 6, 5, 5, 2},
@@ -20,7 +26,7 @@ static const int reel_cnts[N_REELS][SYM_SET_SIZE] = {
     {4, 4, 4, 4, 6, 6, 6, 6, 8, 0, 2},
     {2, 4, 5, 4, 7, 7, 6, 6, 7, 0, 2}};
 
-// FIXED REEL DEFINITIONS
+// Fixed reel definitions (used only when use_fixed_reels == TRUE)
 static symbol fr_0[47] = {CL, SF, LM, CL, BU, LT, LH, LH, LH, BO, LH, LO, TU, TU, BO, SG, TU, WS, LM, WS, SG, CL, TU,
                           BO, BO, LM,
                           SG, LT, SF, LH, SF, BU, CL, SF, CL, CL, BO, SG, LO, LM, BO, BU, TU, SG, SF, TU, BU};
@@ -43,16 +49,24 @@ static symbol fr_4[50] = {SG, LH, SF, SG, LH, BO, CL, SF, SF, CL, SG, SF, TU, LM
                           TU, SG, TU, CL, SF, BO, TU, LM, SF, LT, LH, SG, LH, LH, BO, TU, BU, LM, CL, SF, WS, CL, WS,
                           CL};
 
-// LOCAL FUNCTIONS
+static bool reels_are_initialized = FALSE;
+
+// PRIVATE FUNCTIONS
+
+// Return a pointer to a reel (symbol[] of capacity elements)
 static symbol *make_reel (size_t capacity)
 {
+  assert(use_fixed_reels == FALSE);
+  assert(capacity > 0);
   symbol *rmem = (symbol *) calloc (capacity, sizeof (int));
   assert(rmem != NULL);
   return (rmem);
 }
 
+// Sets the reels[] with pointers to each fixed reel
 static void set_fixed_reels ()
 {
+  assert(use_fixed_reels == TRUE);
   reels[0] = fr_0;
   reels[1] = fr_1;
   reels[2] = fr_2;
@@ -60,14 +74,21 @@ static void set_fixed_reels ()
   reels[4] = fr_4;
 }
 
+// Populate a single pre-allocated reel with the correct number of symbols per reel.
+// Should only be called when use_fixed_reels is FALSE
+// Params: reel -> pointer to a dynamically created reel from make_reel()
+//         reel_idx -> 0 based index of the reel to populate
 static void populate_reel (symbol *reel, int reel_idx)
 {
+  assert(use_fixed_reels == FALSE);
   assert (reel != NULL);
   assert ((reel_idx > -1) && (reel_idx < N_REELS));
   symbol *rptr = reel;
   for (int i = 0; i < SYM_SET_SIZE; i++)
     {
+      // Get symbol cnt for each symbol for this reel
       int cnt = reel_cnts[reel_idx][i];
+      // Duplicate the symbol cnt times
       for (int j = 0; j < cnt; j++)
         {
           *rptr = i;
@@ -75,7 +96,9 @@ static void populate_reel (symbol *reel, int reel_idx)
         }
     }
 }
-
+// Print out a single reel using 2 char codes.
+// Params: reel -> pointer to the reel to be printed.
+//         reel_idx -> integer between 0 and N_REELS-1, used to get reel size from reel_sizes[]
 static void print_reel (symbol *reel, int reel_idx)
 {
   assert(reel != NULL);
@@ -90,14 +113,19 @@ static void print_reel (symbol *reel, int reel_idx)
 // Utility function to swap elements `a[i]` and `a[j]` in an array
 static void swap (symbol a[], int i, int j)
 {
+  assert(a != NULL);
+  assert((i >= 0) && (j >= 0));
   symbol temp = a[i];
   a[i] = a[j];
   a[j] = temp;
 }
 
 // Function to shuffle an array `a[]` of `n` elements
+// Implements the Fisher-Yates algorithm
 static void shuffle_reel (symbol a[], int n)
 {
+  assert(a != NULL);
+  assert((n >= 0) && (n <= MAX_REEL_SIZE));
   // read array from the highest index to lowest
   for (int i = n - 1; i >= 1; i--)
     {
@@ -111,6 +139,9 @@ static void shuffle_reel (symbol a[], int n)
 
 // PUBLIC FUNCTIONS
 
+// Initializes all reels before use. Call this function 1st.
+// If use_fixed_reels == FALSE, be sure to call free_reels() when finished with them
+// to free up allocated heap memory.
 void init_reels ()
 {
   if (use_fixed_reels)
@@ -121,15 +152,19 @@ void init_reels ()
     {
       for (int i = 0; i < N_REELS; i++)
         {
-          symbol *reel = make_reel (reel_sizes[i]);
-          populate_reel (reel, i);
-          reels[i] = reel;
+          symbol *reel = make_reel (reel_sizes[i]); // Create a single reel of correct size
+          populate_reel (reel, i); // Populate this reel with correct number of symbols.
+          shuffle_reel (reel, reel_sizes[i]);
+          reels[i] = reel; // Store a pointer to this reel in the reels[] array.
         }
     }
+  reels_are_initialized = TRUE;
 }
 
+// Return a 2 char C string pointer equivalent to its symbol_t enum value
 char *print_sym (symbol i)
 {
+  assert((i >= WS) && (i <= LT));
   char *ch;
   switch (i)
     {
@@ -172,21 +207,28 @@ char *print_sym (symbol i)
   return (ch);
 }
 
+// Print all reels in the reels[] array
 void print_reels ()
 {
+  assert(reels_are_initialized);
   printf ("\n");
   for (int i = 0; i < N_REELS; i++)
     print_reel (reels[i], i);
 }
 
+// Free dynamically allocated heap memory when use_fixed_reels global variable is FALSE
 void free_reels ()
 {
+  assert(use_fixed_reels == FALSE);
   for (int i = 0; i < N_REELS; i++)
     free (reels[i]);
+  reels_are_initialized = FALSE;
 }
 
+// Randomize the position of all symbols of all reels in place.
 void shuffle_reels ()
 {
+  assert(reels_are_initialized);
   for (int i = 0; i < N_REELS; i++)
     shuffle_reel (reels[i], reel_sizes[i]);
 }
